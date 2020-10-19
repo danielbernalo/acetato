4795ecfea45193a417c0208ee3337f210cc0c4c5
Daniel Bernal <danielbernal@nuevoeste.com>
2020-10-19 11:36:20 -0300

    Init commit

---
+++ src/main/java/com/nxastudios/acetato/delivery/http/Application.java
@@ -0,0 +1,7 @@
+package com.nxastudios.acetato.delivery.http;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Made with ♥ by Daniel Bernal");
+    }
+}
